47a3e5b7f2fb73cd895f56602f95b2768836b155
Daichi-11 <dai77yama@gmail.com>
2024-02-18 20:23:54 +0900

    add: contact section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="#">dai77yama@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/daichi-yamamoto-3885182a3/">Linkedin</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Daichi-11">Github</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/blog.png")} alt="Blog icon" />
+          <a href="https://daichi.hashnode.dev/">Tech Blog</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
